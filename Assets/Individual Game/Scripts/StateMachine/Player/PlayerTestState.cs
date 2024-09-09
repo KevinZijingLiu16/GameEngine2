cedf5f2ce7fa5f9ccf6604e38acabdae5cff059d
ZIJING, LIU <ae0672@student.jamk.fi>
2024-09-09 15:14:30 +0300

    Make Movement relative to camera by adding some code.
    make sure the movement of the player can be the same as the camera view direction, ( moving forward will be always the camara facing direction)

--- Assets/Individual Game/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Individual Game/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -15,10 +15,8 @@ public class PlayerTestState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        Vector3 movement = new Vector3();
-        movement.x = stateMachine.InputReader.MovementValue.x;
-        movement.z = stateMachine.InputReader.MovementValue.y;
-        movement.y = 0;
+        Vector3 movement = CalculateMovement();
+       
        
        stateMachine.Controller.Move(movement * deltaTime * stateMachine.FreeLookMovementSpeed);
 
@@ -41,5 +39,21 @@ public class PlayerTestState : PlayerBaseState
         
     }
 
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.InputReader.MovementValue.y + 
+            right * stateMachine.InputReader.MovementValue.x;
+
+
+    }
    
 }
