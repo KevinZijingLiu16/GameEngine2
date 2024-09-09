dbf5622d9f094cc059caaf8dd4a1e4bc8fd47698
ZIJING, LIU <ae0672@student.jamk.fi>
2024-09-09 11:03:59 +0300

    Add Some Animation Asset from Mixamo

--- Assets/Individual Game/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Individual Game/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -4,36 +4,28 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer;
+   
 
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { } 
 
     public override void Enter()
     {
-        stateMachine.InputReader.JumpEvent += OnJump;
-        Debug.Log("Enter");
+        
     }
 
     public override void Tick(float deltaTime)
     {
-       
 
 
-        timer += deltaTime;
-
-        Debug.Log(timer);
+        Debug.Log(stateMachine.InputReader.MovementValue);
 
         
     }
 
     public override void Exit()
     {
-        stateMachine.InputReader.JumpEvent -= OnJump;
-        Debug.Log("Exit");
+        
     }
 
-    private void OnJump()
-    {
-        stateMachine.SwitchState(new PlayerTestState(stateMachine));
-    }
+   
 }
