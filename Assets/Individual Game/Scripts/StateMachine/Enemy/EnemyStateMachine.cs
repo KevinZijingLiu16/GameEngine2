8c1dbb486f3909beaca8540c000f94b31794892d
ZIJING, LIU <ae0672@student.jamk.fi>
2024-11-01 20:56:43 +0200

    Start Enemy state
    create the beginning of
    Enemy State Maching,
    enemy base state
    enemy idle state
    
    add of logic of chasing player
    
    import the model and animation of enemy.

---
+++ Assets/Individual Game/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine : StateMachine
+{
+    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public float PlayerChasingRange { get; private set; }
+
+    [field: SerializeField] public CharacterController Controller { get; private set; }
+    [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
+
+    public GameObject Player { get; private set; }
+
+    private void Start()
+    {
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        SwitchState(new EnemyIdleState(this));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, PlayerChasingRange);
+    }
+}
